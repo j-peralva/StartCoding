e953c27441d4fd1585b60ed6c67092b40065e80b
Jefferson Peralva Machiqueira <32903179+j-peralva@users.noreply.github.com>
2022-01-06 01:16:31 -0300

    Resolução de exercícios

---
+++ ResolucaoExercicios/AD1/2021-1/Questão 1/Solucao1.py
@@ -0,0 +1,14 @@
+from math import pi
+
+
+distancia_km = int(input("Entre com a distância em quilômetros: "))
+diameter_cm = int(input("Entre com o diâmetro em centímetros: "))
+diameter_km = diameter_cm * 10**-5
+comprimento = round(pi, 3) * diameter_km
+numero_voltas = distancia_km / comprimento
+m, n = tuple(map(float, f"{numero_voltas:e}".split("e")))
+if m >= 3.16:
+    n += 1
+print(f"Distância percorrida: {distancia_km} km")
+print(f"Diâmetro da roda: {diameter_cm} cm")
+print(f"Ordem de grandeza da quantidade de voltas efetuadas: 10 elevado a {int(n)}")
