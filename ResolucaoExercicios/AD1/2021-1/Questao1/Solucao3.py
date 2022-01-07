297c02bfbdc1dfa00d359be1fbebdbd0eee97121
Jefferson Peralva Machiqueira <32903179+j-peralva@users.noreply.github.com>
2022-01-07 19:42:36 -0300

    em breve nova solução

---
+++ ResolucaoExercicios/AD1/2021-1/Questao1/Solucao3.py
@@ -0,0 +1,7 @@
+"""
+:author 'Jefferson Peralva Machiqueira':
+Link para o vídeo: em breve
+A solução apresentada atende aos requisitos da disciplina Fundamentos de Programação? Sim
+
+Solução 3 em breve
+"""
